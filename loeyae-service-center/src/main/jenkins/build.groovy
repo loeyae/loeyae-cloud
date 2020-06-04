94960aa1fe76346ffddb5c47714f90673b5e7e43
ZhangYi <loeyae@gmail.com>
2020-06-04 19:54:05 +0800

    调整build参数

--- loeyae-service-center/src/main/jenkins/build.groovy
+++ loeyae-service-center/src/main/jenkins/build.groovy
@@ -12,41 +12,30 @@ node {
     }
 
     stage("Package") {
-        if ((PACKAGE_BY_STABLE && currentBuild.resultIsBetterOrEqualTo("SUCCESS")) ||
-                !PACKAGE_BY_STABLE) {
-            withCredentials([dockerCert(credentialsId: 'docker-client', variable: 'DOCKER_CERT_PATH')]) {
-                sh """
-                    cd loeyae-service-center/
-                    mvn -f pom.xml clean package -Dautoconfig.skip=true -Dmaven.test.skip=true
-                   """
-            }
-
-        } else {
-            echo "Task FAILURE, Skip package"
+        withCredentials([dockerCert(credentialsId: 'docker-client', variable: 'DOCKER_CERT_PATH')]) {
+            sh """
+                cd loeyae-service-center/
+                mvn -f pom.xml clean package -Dautoconfig.skip=true -Dmaven.test.skip=true
+               """
         }
     }
     stage("Image push") {
         def latestTag = "hub.bys.cd/library/loeyae_service_center:latest"
-        if ((PACKAGE_BY_STABLE && currentBuild.resultIsBetterOrEqualTo("SUCCESS")) ||
-                !PACKAGE_BY_STABLE) {
-            withCredentials([dockerCert(credentialsId: 'docker-client', variable: 'DOCKER_CERT_PATH')]) {
-                try {
-                    sh """
-                  docker tag loeyae_service_center:latest $latestTag
-                  docker push $latestTag
-                  """
-                }
-                catch (exc) {
-                    println("Push image failure")
-                    print(exc.getMessage())
-                    currentBuild.result = 'FAILURE'
-                }
+        withCredentials([dockerCert(credentialsId: 'docker-client', variable: 'DOCKER_CERT_PATH')]) {
+            try {
                 sh """
-                docker rmi $latestTag
-                """
+              docker tag loeyae_service_center:latest $latestTag
+              docker push $latestTag
+              """
             }
-        } else {
-            echo "Task FAILURE, Skip image push"
+            catch (exc) {
+                println("Push image failure")
+                print(exc.getMessage())
+                currentBuild.result = 'FAILURE'
+            }
+            sh """
+            docker rmi $latestTag
+            """
         }
     }
 }
