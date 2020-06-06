803df6003347ecdd4f5bc03db0095af4d4764f03
zhangyi <zhangyi07@beyondsoft.com>
2020-06-06 16:36:48 +0800

    增加fastdfs服务端部署

---
+++ loeyae-files-server/src/main/jenkins/fastdfs/deploy.groovy
@@ -0,0 +1,18 @@
+node {
+    stage("Checkout") {
+        checkout(
+                [
+                        $class: 'GitSCM',
+                        branches: [[name: "origin/master"]],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [], submoduleCfg: [],
+                        userRemoteConfigs: [[credentialsId: 'gitea-user-name', url: 'http://119.4.240.179:31080/loeyae/loeyae-cloud.git']]
+                ]
+        )
+    }
+    stage("deploy") {
+        def source = "loeyae-files-server/src/main/jenkins/fastdfs/loeyae-fastdfs.yml"
+        sh "sed -e 's#{TAG}#${buildId}#g;s#{ENV}#test#g' ${source} > deployment.yml"
+        sh "kubectl apply -f deployment.yml"
+    }
+}
