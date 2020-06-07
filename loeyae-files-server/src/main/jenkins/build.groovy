7b386d66bb87c0062744810de0be1197f097d840
zhangyi <zhangyi07@beyondsoft.com>
2020-06-07 12:53:32 +0800

    更新build地址

--- loeyae-files-server/src/main/jenkins/build.groovy
+++ loeyae-files-server/src/main/jenkins/build.groovy
@@ -14,7 +14,7 @@ node {
     stage("Package") {
         withCredentials([dockerCert(credentialsId: 'docker-client', variable: 'DOCKER_CERT_PATH')]) {
             sh """
-                cd loeyae-gateway/
+                cd loeyae-files-server/
                 mvn -f pom.xml clean package -Dautoconfig.skip=true -Dmaven.test.skip=true
                """
         }
