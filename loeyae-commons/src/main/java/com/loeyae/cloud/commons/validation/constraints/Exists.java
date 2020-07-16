4944378c8a35a580ac2e3b0bec253c320430f216
ZhangYi <loeyae@gmail.com>
2020-07-17 00:24:19 +0800

    增加语言包

--- loeyae-commons/src/main/java/com/loeyae/cloud/commons/validation/constraints/Exists.java
+++ loeyae-commons/src/main/java/com/loeyae/cloud/commons/validation/constraints/Exists.java
@@ -29,7 +29,7 @@ public @interface Exists {
 
     boolean allowedZero() default false;
 
-    String message() default "{javax.validation.constraints.NotNull.message}";
+    String message() default "{loeyae.validation.constraints.Exists.message}";
 
     Class<?>[] groups() default { };
 
